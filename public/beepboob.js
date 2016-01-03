d3044e0e56fa660c2ccdb82b010658769667a225
Bruno Teixeira <bteixeira666@gmail.com>
2016-01-03 00:46:02 +0000

    Added static page. Added data snapshot.

---
+++ public/beepboob.js
@@ -0,0 +1,25 @@
+/**
+ * Created by bruno on 02.01.16.
+ */
+$(function () {
+
+    var $container =$('<div class="container"></div>');
+    $('body').append($container);
+
+    $container.text('Loading');
+
+    var i = 1;
+    var id = setInterval(function () {
+        i++;
+        $container.text('Loading' + new Array(i).join('.'))
+    }, 500);
+
+    $.get('http://localhost:9009', function (response) {
+
+        clearInterval(id);
+
+        $container.text('Choose:');
+        $container.append('<pre>' + response + '</pre>');
+    });
+
+});
