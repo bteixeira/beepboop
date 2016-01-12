c221f067e59e36749390ff1af515fd0ffbd69520
Bruno Teixeira <bteixeira666@gmail.com>
2016-01-12 21:29:37 +0100

    Update babepedia gellery URL crawler

--- babepedia/getGalleryUrls.js
+++ babepedia/getGalleryUrls.js
@@ -1,44 +1,26 @@
-
 var fs = require('fs');
 var cheerio = require('cheerio');
 var utils = require('../utils');
 
-utils.readAll(process.stdin, function (content) {
-    var list = content.split('\n');
-    var slug;
-    var i = 0;
-    function getIt () {
-        if (i >= list.length) {
-            console.log('All work done.');
-            return;
-        }
-        slug = list[i++];
-        if (!slug) {
-            getIt();
-            return;
-        }
-        var filename = __dirname + '/pages/' + slug + '.htm';
-        console.log('Opening file "' + filename + '"');
-        utils.readAll(filename, function (content) {
-            var filename = __dirname + '/gallery_urls/' + slug + '.txt';
-            console.log('File opened, writing to "' + filename + '"...');
-            var ws = fs.createWriteStream(filename);
-            var $ = cheerio.load(content);
-            function writeAll($links) {
-                $links.each(function () {
-                    ws.write($(this).attr('href') + '\n');
-                });
-            }
-            var name = $('#bioarea h1').text().trim();
-            //writeAll($('a[rel="gallery"]'));
-            //writeAll($('.thumbshot a')); // UNKNOWN
-
-            // NOT SINGLES
-            writeAll($('h2').filter(function(){return $(this).text().trim() === name + ' Galleries';}).parent().find('a'));
-            ws.end();
-            console.log('Done.');
-            getIt();
-        });
-    }
-    getIt();
+utils.readEachLine(process.stdin, function (slug, next) {
+    var filename = __dirname + '/pages/' + slug + '.htm';
+    console.log('Opening file "' + filename + '"');
+    var page = fs.readFileSync(filename);
+    var $ = cheerio.load(page);
+    filename = __dirname + '/gallery_urls/' + slug + '.txt';
+    console.log('File opened, writing to "' + filename + '"...');
+    var ws = fs.createWriteStream(filename);
+    var name = $('#bioarea h1').text().trim();
+    var $links = $('h2').filter(function(){return $(this).text().trim() === name + ' Galleries';}).parent().find('a');
+    writeAll($links, $, ws);
+    ws.end(function () {
+        console.log('Done.');
+        next();
+    });
 });
+
+function writeAll($links, $, ws) {
+    $links.each(function () {
+        ws.write($(this).attr('href') + '\n');
+    });
+}
