898400572273b5c2b89fd93d96aa13c72a0f2c9e
Bruno Teixeira <bteixeira666@gmail.com>
2016-10-05 20:47:37 +0200

    Added SASS preprocessor. Removed old prototype client.

--- web/app.js
+++ web/app.js
@@ -1,12 +1,21 @@
 var path = require('path');
 
 var express = require('express');
+var sassMiddleware = require('node-sass-middleware');
 
 var app = express();
 
 app.use('/', require('./app-client'));
 app.use('/curator', express.static(path.resolve(__dirname, 'curator')));
 app.use('/api', require('./api'));
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'stylesheets'),
+    dest: path.join(__dirname, 'public', 'css-dist'),
+    debug: true,
+    outputStyle: 'compressed'//,
+    // prefix:  '/prefix'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+    , prefix: '/css'
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen('9000');
